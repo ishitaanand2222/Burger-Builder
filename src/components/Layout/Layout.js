701e83eadf23ad6665d7c70fe3bf4928ab31be67
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-07-10 14:40:34 +0530

    Adding Logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Auxillary from "../../hoc/Auxillary";
 import classes from './Layout.module.css'
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const Layout = (props) =>{
     return(
         <Auxillary>
-            <div>Toolbar </div>
+            <Toolbar />
             <main className = {classes.Content}>
                 {props.children}
             </main>
