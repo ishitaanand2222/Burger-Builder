f6b279817b90af01486ffe162fad80f7a854d62e
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-07-19 16:54:33 +0530

    Fetching Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const Orders = () => {
 
+    const[orders, setOrders] = useState([]);
+    const[loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get('/orders.json')
+        .then((response) => {
+            const fetchedOrders = [];
+            for(let key in response.data){
+                fetchedOrders.push({
+                    ...response.data[key],
+                    id: key
+                });
+            }
+            setLoading(false);
+            setOrders(fetchedOrders);
+        })
+        .catch((err) => {
+            setLoading(false);
+        })
+    },[])
+     
     return(
         <div>
             <Order/>
@@ -11,5 +34,5 @@ const Orders = () => {
     )
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
 
